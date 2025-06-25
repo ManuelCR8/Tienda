d96918895ba0b57147a36483bba553efe579afae
chaco <chaco@DESKTOP-O3K7GFF>
2025-06-24 18:26:55 -0600

    Avance semana 6

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public interface CategoriaService {
     public List<Categoria> getCategorias(boolean activos);
+
+    public void save(Categoria categoria);
+
+    public void delete(Categoria categoria);
+
+    public Categoria getCategoria(Categoria categoria);
 }
---
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -0,0 +1,15 @@
+
+package com.tienda.service;
+
+import com.tienda.domain.Producto;
+import java.util.List;
+
+public interface ProductoService {
+    public List<Producto> getProductos(boolean activos);
+
+    public void save(Producto producto);
+
+    public void delete(Producto producto);
+
+    public Producto getProducto(Producto producto);
+}
